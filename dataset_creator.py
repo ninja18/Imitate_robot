e5bfb99f490b7b33eb709bd0e41346854a04d9a2
ninja18 <newninja18@gmail.com>
2019-03-12 19:34:34 +0530

    Half model with dataset_collector

--- dataset_creator.py
+++ dataset_creator.py
@@ -3,16 +3,20 @@ import cv2 as cv
 import imageio
 import os
 from pathlib import Path
+import time
 
 def create_gif(out_file):
-    start = input("Press Space to start recording")
+    start = input("Press Enter to start recording")
     print("Press Space to stop recording")
-    if(start == " "):
+    if(start == ""):
         print("GO....!!!")
         #cap = cv.VideoCapture("test_code/swainas.mp4")
         cap = cv.VideoCapture(0)
-        cap.set(cv.CAP_PROP_FPS,10)
+        cap.set(cv.CAP_PROP_FPS,10.0)
+        fps = cap.get(cv.CAP_PROP_FPS)
+        print(fps)
         images = []
+        start = time.time()
         while(True):
             ret, frame = cap.read()
             if (not ret) or (cv.waitKey(1) & 0xFF == ord(' ')):
@@ -23,13 +27,14 @@ def create_gif(out_file):
             images.append(res)
             cv.imshow('frame',frame)
             #cv.imshow('resized frame',res)
-
+        end = time.time()
+        print(end - start)
         cap.release()
         cv.destroyAllWindows()
         imageio.mimsave(out_file,images)
-        print("Saved...")
+        print(f"Saved... {len(images)}")
         
-if(__name == '__main__'):      
+if(__name__ == '__main__'):      
     print("This is the Dataset maker")
     opt = input("\tPress 1 to capture videos\n\tPress 2 to exit")
     while(opt != '2'):
