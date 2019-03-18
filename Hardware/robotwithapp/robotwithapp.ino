17cb6c00db977c2e6753682e76b07d6f7016f5d4
ninja18 <newninja18@gmail.com>
2019-03-18 21:18:28 +0530

    Completed dataset creator in HW & SW

--- Hardware/robotwithapp/robotwithapp.ino
+++ Hardware/robotwithapp/robotwithapp.ino
@@ -1,6 +1,9 @@
-//#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
 #include <Servo.h>
 #include<Ticker.h>
+
+const char* ssid = "NinjA";
+const char* password = "nooooooo";
 
 Servo servo01;
 Servo servo02;
@@ -9,20 +12,21 @@ Servo servo04;
 Servo servo05;
 Servo servo06;
 
+WiFiServer server(80);
 Ticker logger;
-//SoftwareSerial Bluetooth(3, 4); // Arduino(RX, TX) - HC-05 Bluetooth (TX, RX)
 
 uint8_t servo1Pos, servo2Pos, servo3Pos, servo4Pos, servo5Pos, servo6Pos; // current position
 uint8_t servo1PPos, servo2PPos, servo3PPos, servo4PPos, servo5PPos, servo6PPos; // previous position
-uint8_t servo01SP[50] {60,60,90,60,60}, servo02SP[50] {150,90,90,120,120}, servo03SP[50] {35,35,35,35,35}, servo04SP[50] {140,140,40,40,40}, servo05SP[50] {85,85,85,85,85}, servo06SP[50] {80,80,80,80,120}; // for storing positions/steps
+uint8_t servo01SP[50], servo02SP[50], servo03SP[50], servo04SP[50], servo05SP[50], servo06SP[50]; // for storing positions/steps
 
 uint8_t servo01Act[100], servo02Act[100], servo03Act[100], servo04Act[100], servo05Act[100], servo06Act[100]; // for actions
 uint8_t speedDelay = 20;
-uint8_t ind = 5;
+uint8_t ind = 0;
 String dataIn = "";
 uint8_t logger_i = 0;
 bool isLog = false;
 bool isRun = true;
+String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nRECIEVED</html>\n";
 
 void log_data(){
   if(isLog){
@@ -43,10 +47,23 @@ void setup() {
   servo04.attach(D8);
   servo05.attach(D3);
   servo06.attach(D4);
-  //Bluetooth.begin(38400); // Default baud rate of the Bluetooth module
-  //Bluetooth.setTimeout(1);
   delay(20);
   Serial.begin(115200);
+
+  // Connect to WiFi network
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  
+  // Start the server
+  server.begin();
+ 
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+  
   // Robot arm initial position
   servo1PPos = 60;
   servo01.write(servo1PPos);
@@ -60,18 +77,28 @@ void setup() {
   servo05.write(servo5PPos);
   servo6PPos = 80;
   servo06.write(servo6PPos);
-  Serial.println("Starting");
   logger.attach(0.1,log_data);
 }
 
 void loop() {
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  // Wait until the client sends some data
+  while(!client.available()){
+    delay(1);
+  }
+  
   // Check for incoming data
-  /*if (Bluetooth.available() > 0) {
-    dataIn = Bluetooth.readString();  // Read the data as string
+    dataIn = client.readStringUntil('H');
+    int l = dataIn.length();
+    client.flush();  // Read the data as string
     
     // If "Waist" slider has changed value - Move Servo 1 to position
-    if (dataIn.startsWith("s1")) {
-      String dataInS = dataIn.substring(2, dataIn.length()); // Extract only the number. E.g. from "s1120" to "120"
+    if (dataIn.startsWith("GET /servo1")) {
+      String dataInS = dataIn.substring(12, l-1); // Extract only the number. E.g. from "s1120" to "120"
       servo1Pos = dataInS.toInt();  // Convert the string into integer
       // We use for loops so we can control the speed of the servo
       // If previous position is bigger then current position
@@ -89,11 +116,13 @@ void loop() {
         }
       }
       servo1PPos = servo1Pos;   // set current position as previous position
+        client.print(s);
+  delay(1);
     }
     
     // Move Servo 2
-    if (dataIn.startsWith("s2")) {
-      String dataInS = dataIn.substring(2, dataIn.length());
+    if (dataIn.startsWith("GET /servo2")) {
+      String dataInS = dataIn.substring(12, l-1);
       servo2Pos = dataInS.toInt();
 
       if (servo2PPos > servo2Pos) {
@@ -109,10 +138,12 @@ void loop() {
         }
       }
       servo2PPos = servo2Pos;
+        client.print(s);
+  delay(1);
     }
     // Move Servo 3
-    if (dataIn.startsWith("s3")) {
-      String dataInS = dataIn.substring(2, dataIn.length());
+    if (dataIn.startsWith("GET /servo3")) {
+      String dataInS = dataIn.substring(12, l-1);
       servo3Pos = dataInS.toInt();
       if (servo3PPos > servo3Pos) {
         for ( int j = servo3PPos; j >= servo3Pos; j--) {
@@ -127,10 +158,12 @@ void loop() {
         }
       }
       servo3PPos = servo3Pos;
+        client.print(s);
+  delay(1);
     }
     // Move Servo 4
-    if (dataIn.startsWith("s4")) {
-      String dataInS = dataIn.substring(2, dataIn.length());
+    if (dataIn.startsWith("GET /servo4")) {
+      String dataInS = dataIn.substring(12, l-1);
       servo4Pos = dataInS.toInt();
       if (servo4PPos > servo4Pos) {
         for ( int j = servo4PPos; j >= servo4Pos; j--) {
@@ -145,10 +178,12 @@ void loop() {
         }
       }
       servo4PPos = servo4Pos;
+        client.print(s);
+  delay(1);
     }
     // Move Servo 5
-    if (dataIn.startsWith("s5")) {
-      String dataInS = dataIn.substring(2, dataIn.length());
+    if (dataIn.startsWith("GET /servo5")) {
+      String dataInS = dataIn.substring(12, l-1);
       servo5Pos = dataInS.toInt();
       if (servo5PPos > servo5Pos) {
         for ( int j = servo5PPos; j >= servo5Pos; j--) {
@@ -163,10 +198,12 @@ void loop() {
         }
       }
       servo5PPos = servo5Pos;
+        client.print(s);
+  delay(1);
     }
     // Move Servo 6
-    if (dataIn.startsWith("s6")) {
-      String dataInS = dataIn.substring(2, dataIn.length());
+    if (dataIn.startsWith("GET /servo6")) {
+      String dataInS = dataIn.substring(12, l-1);
       servo6Pos = dataInS.toInt();
       if (servo6PPos > servo6Pos) {
         for ( int j = servo6PPos; j >= servo6Pos; j--) {
@@ -181,80 +218,94 @@ void loop() {
         }
       }
       servo6PPos = servo6Pos; 
+        client.print(s);
+  delay(1);
     }
     // If button "SAVE" is pressed
-    if (dataIn.startsWith("SAVE")) {
-      servo01SP[index] = servo1PPos;  // save position into the array
-      servo02SP[index] = servo2PPos;
-      servo03SP[index] = servo3PPos;
-      servo04SP[index] = servo4PPos;
-      servo05SP[index] = servo5PPos;
-      servo06SP[index] = servo6PPos;
-      index++;                        // Increase the array index
+    if (dataIn.startsWith("GET /save")) {
+      servo01SP[ind] = servo1PPos;  // save position into the array
+      servo02SP[ind] = servo2PPos;
+      servo03SP[ind] = servo3PPos;
+      servo04SP[ind] = servo4PPos;
+      servo05SP[ind] = servo5PPos;
+      servo06SP[ind] = servo6PPos;
+      ind++;
+      isRun = true;
+        client.print(s);
+  delay(1);
     }
     // If button "RUN" is pressed
-    if (dataIn.startsWith("RUN")) {*/
+    if (dataIn.startsWith("GET /run")) {
+       client.print(s);
+  delay(1);
      if(isRun){
      isLog = true;
+     while(!Serial.available){
+      }
+     if(Serial.read() == " "){
       runservo(); // Automatic mode - run the saved steps 
       isRun = false;
       isLog = false;
-      Serial.print("Actions collected is ");
-      Serial.println(logger_i);
       for(uint8_t i = 0; i < logger_i;i++)
       {
-        Serial.print(i);
-        Serial.print(" : ");
         Serial.print(servo01Act[i]);
-        Serial.print(", ");
+        Serial.print(" ");
         Serial.print(servo02Act[i]);
-        Serial.print(", ");
+        Serial.print(" ");
         Serial.print(servo03Act[i]);
-        Serial.print(", ");
+        Serial.print(" ");
         Serial.print(servo04Act[i]);
-        Serial.print(", ");
+        Serial.print(" ");
         Serial.print(servo05Act[i]);
-        Serial.print(", ");
+        Serial.print(" ");
         Serial.print(servo06Act[i]);
-        Serial.println("    OVer...."); //,servo02Act[i],servo03Act[i],servo04Act[i],servo05Act[i],servo06Act[i]);
-      }
+        Serial.println();
+        }
      }
-    /*}
+     }
+    }
     // If button "RESET" is pressed
-    if ( dataIn == "RESET") {
+    if ( dataIn == "GET /reset ") {
       memset(servo01SP, 0, sizeof(servo01SP)); // Clear the array data to 0
       memset(servo02SP, 0, sizeof(servo02SP));
       memset(servo03SP, 0, sizeof(servo03SP));
       memset(servo04SP, 0, sizeof(servo04SP));
       memset(servo05SP, 0, sizeof(servo05SP));
       memset(servo06SP, 0, sizeof(servo06SP));
-      index = 0;  // Index to 0
+      ind = 0;  // ind to 0
+        client.print(s);
+  delay(1);
     }
-  }*/
-}
+  // Send the response to the client
+  }
 
 // Automatic mode custom function - run the saved steps
 void runservo() {
-  //while (dataIn != "RESET") {  // Run the steps over and over again until "RESET" button is pressed
-    for (uint8_t i = 0; i <= ind - 2; i++) { // Run through all steps(index)
-      /*if (Bluetooth.available() > 0) {      // Check for incomding data
-        dataIn = Bluetooth.readString();
-        if ( dataIn == "PAUSE") {           // If button "PAUSE" is pressed
-          while (dataIn != "RUN") {         // Wait until "RUN" is pressed again
-            if (Bluetooth.available() > 0) {
-              dataIn = Bluetooth.readString();
-              if ( dataIn == "RESET") {     
+  WiFiClient client = server.available(); // Run the steps
+    for (uint8_t i = 0; i <= ind - 2; i++) {
+      // Run through all steps(ind)
+      if (client.available() > 0) {      // Check for incomding data
+        dataIn = client.readStringUntil('H');
+        client.flush();
+        client.print(s);
+        if ( dataIn == "GET /pause ") {           // If button "PAUSE" is pressed
+          while (dataIn != "GET /run ") {         // Wait until "RUN" is pressed again
+            if (client.available() > 0) {      // Check for incomding data
+        dataIn = client.readStringUntil('H');
+        client.flush();
+        client.print(s);
+              if ( dataIn == "GET /reset ") {   
                 break;
               }
             }
           }
         }
         // If speed slider is changed
-        if (dataIn.startsWith("ss")) {
-          String dataInS = dataIn.substring(2, dataIn.length());
+        if (dataIn.startsWith("GET /speed")) {
+          String dataInS = dataIn.substring(11, dataIn.length()-1);
           speedDelay = dataInS.toInt(); // Change servo speed (delay time)
         }
-      }*/
+      }
       // Servo 1
       if (servo01SP[i] == servo01SP[i + 1]) {
         
@@ -369,7 +420,8 @@ void runservo() {
         }
       }
     }
-  //}
+    Serial.println("END");
+    Serial.println(logger_i);
 }
 
 
--- dataset_creator.py
+++ dataset_creator.py
@@ -4,18 +4,19 @@ import imageio
 import os
 from pathlib import Path
 import time
+import serial
+import pickle
 
-def create_gif(out_file):
+
+def create_human_gif(out_file):
+    images = []
     start = input("Press Enter to start recording")
     print("Press Space to stop recording")
     if(start == ""):
         print("GO....!!!")
-        #cap = cv.VideoCapture("test_code/swainas.mp4")
         cap = cv.VideoCapture(0)
         cap.set(cv.CAP_PROP_FPS,10.0)
         fps = cap.get(cv.CAP_PROP_FPS)
-        print(fps)
-        images = []
         start = time.time()
         while(True):
             ret, frame = cap.read()
@@ -33,6 +34,55 @@ def create_gif(out_file):
         cv.destroyAllWindows()
         imageio.mimsave(out_file,images,duration=0.1)
         print(f"Saved... Frames : {len(images)}")
+
+        
+def create_robot_gif(out_file):
+    ser = serial.Serial("/dev/ttyUSB0",115200)
+    images = []
+    actions = []
+    robot_dict = {}
+    states = [[60,150,35,140,85,80]]
+    
+    start = input("Press Enter to start recording")
+    print("Press Space to stop recording")
+    
+    if(start == ""):
+        ser.write(b' ')
+        print("GO....!!!")
+        cap = cv.VideoCapture(0)
+        cap.set(cv.CAP_PROP_FPS,10.0)
+        fps = cap.get(cv.CAP_PROP_FPS)
+        start = time.time()
+        while(True):
+            ret, frame = cap.read()
+            if (not ret) or (ser.readline() == "END"):
+                print("Over...")
+                ind = ser.readline()
+                break
+                
+            res = cv.resize(frame,(100,90))
+            images.append(res)
+            cv.imshow('frame',frame)
+            #cv.imshow('resized frame',res)
+            
+        end = time.time()
+        print(end - start)
+        for i in range(ind):
+            act_i = ser.readline()
+            act_i = [int(x) for x in act_i.split(' ')]
+            actions.append(act_i)
+            states.append(act_i)
+        cap.release()
+        cv.destroyAllWindows()
+        ser.close()
+        imageio.mimsave(out_file,images,duration=0.1)
+        print(f"Saved... Frames : {len(images)}")
+        robot_dict['demoA'] = actions
+        robot_dict['demoS'] = states
+        dict_file = open(out_file.with_suffix(".pkl"),w)
+        pickle.dump(robot_dict,dict_file)
+        dict_file.close()
+        print("Pickle file saved...")
         
 if(__name__ == '__main__'):      
     print("This is the Dataset maker")
@@ -40,9 +90,21 @@ if(__name__ == '__main__'):
     while(opt != '2'):
         ob_num = input("Enter the object number")
         task_num = input("Enter the task number")
+        isRobot = input("Enter 'y' if this is robot")
         ob_folder = Path(f"dataset/placing/object{ob_num}")
         if not os.path.exists(ob_folder):
             os.makedirs(ob_folder)
-        out_file = ob_folder / f"task{task_num}.gif"
-        create_gif(out_file)
+        
+        if(isRobot == 'y'):
+            task_folder = ob_folder / "robot"
+            create_data = create_robot_gif
+        else:
+            task_folder = ob_folder / "human"
+            create_data = create_human_gif
+            
+        if not os.path.exists(task_folder):
+            os.makedirs(task_folder)
+            
+        out_file = task_folder / f"task{task_num}.gif"
+        create_data(out_file)
         opt = input("\tPress 1 to capture videos\n\tPress 2 to exit")
